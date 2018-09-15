1034476e4efc217d3c81c0cafdb07b3a515c2fff
Navon Francis <navonfrancis@gmail.com>
2018-09-15 02:46:14 -0400

    yeet

--- saywhat-frontend/src/App.js
+++ saywhat-frontend/src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Components/Home';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">SayWhatttttt</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Route exact path="/" component={Home} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ saywhat-frontend/src/Components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+class Home extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+      }
+  }
+
+  render() {
+    return(
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Home;
