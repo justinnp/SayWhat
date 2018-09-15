ac64a4a7bbe708af930c201e9aaf7c3089af3642
Justin Powell <justinmpowell@knights.ucf.edu>
2018-09-15 12:12:48 -0400

    grid

--- saywhat-frontend/src/App.js
+++ saywhat-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
 import CreationModal from './Components/CreationModal';
+import Hub from './Components/Hub';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/creation" component={CreationModal} />
+          <Route path="/home" component={Hub} />
       </Switch>
     </BrowserRouter>
   );
